fbe5a4c0a80e9ec6c44d34390869e63acc601aee
Petros Schutz Schilling <petros.schilling@gmail.com>
2020-04-02 14:51:26 +1100

    Implemented logic for comparison of Lender Tabs

--- src/model/executive_summary.py
+++ src/model/executive_summary.py
@@ -3,11 +3,15 @@ import numpy
 import pandas
 import xlrd
 import copy
+import hashlib
 
-from src.model.taxinvoice import (TaxInvoice, new_error, OUTPUT_DIR_EXEC_SUMMARY, get_header_format,
-                                  get_error_format)
+from src.model.taxinvoice import (TaxInvoice, InvoiceRow, ENCODING, new_error, OUTPUT_DIR_EXEC_SUMMARY,
+                                  get_header_format, get_error_format)
 from src import utils as u
 from src.utils import bcolors
+
+HEADER_LENDER = ['Bank', 'Bank Detailed Name', 'Settlement Amount', 'Commission Amount (Excl GST)',
+                 'GST', 'Commission Amount Inc GST']
 
 
 class ExecutiveSummary(TaxInvoice):
@@ -25,17 +29,26 @@ class ExecutiveSummary(TaxInvoice):
         self.datarows_branch_fee_summary = {}
         self.datarows_broker_summary = {}
         self.datarows_broker_fee_summary = {}
-        self.summary_errors = []
+        self.datarows_lender_upfront = {}
+        self.datarows_lender_trail = {}
+        self.datarows_lender_vbi = {}
+        self.summary_errors = []  # List of errors found during the comparison
         self.pair = None
-        self.margin = 0
+        self.margin = 0  # margin of error acceptable for numeric comprisons
         self.parse()
 
     def parse(self):
         xl = pandas.ExcelFile(self.full_path)
+        self.datarows_lender_upfront = self.parse_lender(xl, 'Lender Upfront Records')
+        self.datarows_lender_trail = self.parse_lender(xl, 'Lender Trail Records')
+        self.datarows_lender_vbi = self.parse_lender(xl, 'Lender VBI Records')
         self.datarows_branch_summary = self.parse_branch(xl, 'Branch Summary Report')
         self.datarows_branch_fee_summary = self.parse_branch(xl, 'Branch Fee Summary Report')
         self.datarows_broker_summary = self.parse_broker(xl, 'Broker Summary Report')
         self.datarows_broker_fee_summary = self.parse_broker(xl, 'Broker Fee Summary Report')
+
+    def parse_lender(self, xl, tab):
+        pass  # TODO
 
     def parse_branch(self, xl, tab):
         rows = {}
@@ -139,29 +152,26 @@ class ExecutiveSummary(TaxInvoice):
         fmt_table_header = get_header_format(workbook)
         fmt_error = get_error_format(workbook)
 
-        self.process_generic(
-            workbook, 'Branch Summary Report',
-            self.datarows_branch_summary,
-            self.pair.datarows_branch_summary,
-            fmt_table_header, fmt_error)
+        self.process_generic(workbook, 'Branch Summary Report', self.datarows_branch_summary,
+                             self.pair.datarows_branch_summary, fmt_table_header, fmt_error)
 
-        self.process_generic(
-            workbook, 'Branch Fee Summary Report',
-            self.datarows_branch_fee_summary,
-            self.pair.datarows_branch_summary,
-            fmt_table_header, fmt_error)
+        self.process_generic(workbook, 'Branch Fee Summary Report', self.datarows_branch_fee_summary,
+                             self.pair.datarows_branch_summary, fmt_table_header, fmt_error)
 
-        self.process_generic(
-            workbook, 'Broker Summary Report',
-            self.datarows_broker_summary,
-            self.pair.datarows_broker_summary,
-            fmt_table_header, fmt_error)
+        self.process_generic(workbook, 'Broker Summary Report', self.datarows_broker_summary,
+                             self.pair.datarows_broker_summary, fmt_table_header, fmt_error)
 
-        self.process_generic(
-            workbook, 'Broker Fee Summary Report',
-            self.datarows_broker_fee_summary,
-            self.pair.datarows_broker_summary,
-            fmt_table_header, fmt_error)
+        self.process_generic(workbook, 'Broker Fee Summary Report', self.datarows_broker_fee_summary,
+                             self.pair.datarows_broker_summary, fmt_table_header, fmt_error)
+
+        self.process_lender(workbook, 'Lender Upfront Records', self.datarows_lender_upfront,
+                            self.pair.datarows_lender_upfront, fmt_table_header, fmt_error)
+
+        self.process_lender(workbook, 'Lender Trail Records', self.datarows_lender_trail,
+                            self.pair.datarows_lender_trail, fmt_table_header, fmt_error)
+
+        self.process_lender(workbook, 'Lender VBI Records', self.datarows_lender_vbi,
+                            self.pair.datarows_lender_vbi, fmt_table_header, fmt_error)
 
         if len(self.summary_errors) > 0:
             workbook.close()
@@ -203,6 +213,68 @@ class ExecutiveSummary(TaxInvoice):
                 self.filename, self.pair.filename, fmt_error, tab)
             row += 1
 
+    def process_lender(self, workbook, tab, datarows, datarows_pair, fmt_table_header, fmt_error):
+        worksheet = workbook.add_worksheet(tab)
+        row = 0
+        col_a = 0
+        col_b = len(HEADER_LENDER) + 1
+
+        for index, item in enumerate(HEADER_LENDER):
+            worksheet.write(row, col_a + index, item, fmt_table_header)
+            worksheet.write(row, col_b + index, item, fmt_table_header)
+        row += 1
+
+        keys_unmatched = set(datarows_pair.keys()) - set(datarows.keys())
+
+        # Code below is just to find the errors and write them into the spreadsheets
+        for key_full in datarows.keys():
+            self_row = datarows[key_full]
+            self_row.margin = self.margin
+
+            pair_row = self.find_pair_row(self_row)
+            self_row.pair = pair_row
+
+            if pair_row is not None:
+                # delete from pair list so it doesn't get matched again
+                del datarows_pair[pair_row.key_full]
+                # Remove the key from the keys_unmatched if it is there
+                if pair_row.key_full in keys_unmatched:
+                    keys_unmatched.remove(pair_row.key_full)
+
+                pair_row.margin = self.margin
+                pair_row.pair = self_row
+                self.summary_errors += LenderExecutiveSummaryRow.write_row(
+                    worksheet, self, pair_row, row, fmt_error, 'right', write_errors=False)
+
+            self.summary_errors += LenderExecutiveSummaryRow.write_row(worksheet, self, self_row, row, fmt_error)
+            row += 1
+
+        # Write unmatched records
+        for key in keys_unmatched:
+            self.summary_errors += LenderExecutiveSummaryRow.write_row(
+                worksheet, self, datarows_pair[key], row, fmt_error, 'right', write_errors=False)
+            row += 1
+
+    def find_pair_row(self, datarows_pair, row):
+        # Match by full_key
+        pair_row = datarows_pair.get(row.key_full, None)
+        if pair_row is not None:
+            return pair_row
+
+        # We want to match by similarity before matching by the key
+        # Match by similarity
+        for _, item in datarows_pair.items():
+            if row.equals(item):
+                return item
+
+        # Match by key
+        for _, item in datarows_pair.items():
+            if row.key == item.key:
+                return item
+
+        # Return None if nothing found
+        return None
+
     def new_error(self, msg, line_a='', line_b='', value_a='', value_b='', tab=''):
         return new_error(self.filename, self.pair.filename, msg, line_a, line_b, value_a, value_b, tab)
 
@@ -217,6 +289,195 @@ class ExecutiveSummary(TaxInvoice):
         if len(val) == 0:
             return val
         return val.split('(')[0].strip()
+
+
+class LenderExecutiveSummaryRow(InvoiceRow):
+
+    def __init__(self, bank, bank_detailed_name, settlement_amount, commission_amount_exc_gst, gst,
+                 commission_amount_inc_gst, row_number):
+        InvoiceRow.__init__(self)
+        self._pair = None
+        self._margin = 0
+
+        self.bank = bank
+        self.bank_detailed_name = bank_detailed_name
+        self.settlement_amount = settlement_amount
+        self.commission_amount_exc_gst = commission_amount_exc_gst
+        self.gst = gst
+        self.commission_amount_inc_gst = commission_amount_inc_gst
+
+        self.row_number = row_number
+
+        self._key = self._generate_key()
+        self._key_full = self._generate_key_full()
+
+    # region Properties
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, k):
+        self._key = k
+
+    @property
+    def key_full(self):
+        return self._key_full
+
+    @key_full.setter
+    def key_full(self, k):
+        self._key_full = k
+
+    @property
+    def pair(self):
+        return self._pair
+
+    @pair.setter
+    def pair(self, pair):
+        self._pair = pair
+
+    @property
+    def margin(self):
+        return self._margin
+
+    @margin.setter
+    def margin(self, margin):
+        self._margin = margin
+
+    @property
+    def equal_bank(self):
+        if self.pair is None:
+            return False
+        return u.sanitize(self.bank) == u.sanitize(self.pair.bank)
+
+    @property
+    def equal_bank_detailed_name(self):
+        if self.pair is None:
+            return False
+        return u.sanitize(self.bank_detailed_name) == u.sanitize(self.pair.bank_detailed_name)
+
+    @property
+    def equal_settlement_amount(self):
+        if self.pair is None:
+            return False
+        return self.compare_numbers(self.settlement_amount, self.pair.settlement_amount)
+
+    @property
+    def equal_commission_amount_exc_gst(self):
+        if self.pair is None:
+            return False
+        return self.compare_numbers(self.commission_amount_exc_gst, self.pair.commission_amount_exc_gst)
+
+    @property
+    def equal_gst(self):
+        if self.pair is None:
+            return False
+        return self.compare_numbers(self.gst, self.pair.gst)
+
+    @property
+    def equal_commission_amount_inc_gst(self):
+        if self.pair is None:
+            return False
+        return self.compare_numbers(self.commission_amount_inc_gst, self.pair.commission_amount_inc_gst)
+    # endregion
+
+    def _generate_key(self, salt=''):
+        sha = hashlib.sha256()
+        sha.update(u.sanitize(self.bank).encode(ENCODING))
+        return sha.hexdigest()
+
+    def _generate_key_full(self, salt=''):
+        sha = hashlib.sha256()
+        sha.update(self.bank.encode(ENCODING))
+        sha.update(self.bank_detailed_name.encode(ENCODING))
+        sha.update(self.settlement_amount.encode(ENCODING))
+        sha.update(self.commission_amount_exc_gst.encode(ENCODING))
+        sha.update(self.gst.encode(ENCODING))
+        sha.update(self.commission_amount_inc_gst.encode(ENCODING))
+        sha.update(str(salt).encode(ENCODING))
+        return sha.hexdigest()
+
+    def equals(self, obj):
+        if type(obj) != LenderExecutiveSummaryRow:
+            return False
+
+        return (
+            u.sanitize(self.bank) == u.sanitize(obj.bank)
+            and u.sanitize(self.bank_detailed_name) == u.sanitize(obj.bank_detailed_name)
+            and self.compare_numbers(self.settlement_amount, obj.settlement_amount, self.margin)
+            and self.compare_numbers(self.commission_amount_exc_gst, obj.commission_amount_exc_gst, self.margin)
+            and self.compare_numbers(self.gst, obj.gst, self.margin)
+            and self.compare_numbers(self.commission_amount_inc_gst, obj.commission_amount_inc_gst, self.margin)
+        )
+
+    @staticmethod
+    def write_row(worksheet, invoice, element, row, fmt_error, side='left', write_errors=True):
+        col = 0
+        if side == 'right':
+            col = 10
+
+        worksheet.write(row, col, element.bank)
+
+        format_ = fmt_error if not element.equal_bank_detailed_name else None
+        worksheet.write(row, col + 1, element.bank_detailed_name)
+
+        format_ = fmt_error if not element.equal_settlement_amount else None
+        worksheet.write(row, col + 2, element.settlement_amount, format_)
+
+        format_ = fmt_error if not element.equal_commission_amount_exc_gst else None
+        worksheet.write(row, col + 3, element.commission_amount_exc_gst, format_)
+
+        format_ = fmt_error if not element.equal_gst else None
+        worksheet.write(row, col + 4, element.gst, format_)
+
+        format_ = fmt_error if not element.equal_commission_amount_inc_gst else None
+        worksheet.write(row, col + 5, element.commission_amount_inc_gst, format_)
+
+        format_ = fmt_error if not element.equal_comments else None
+        worksheet.write(row, col + 6, element.comments, format_)
+
+        errors = []
+        line_a = element.row_number
+        description = f"Bank: {element.bank}"
+        if element.pair is not None:
+            line_b = element.pair.row_number
+            if write_errors:
+                if not element.equal_bank_detailed_name:
+                    msg = 'Detailed Bank Name does not match'
+                    errors.append(new_error(
+                        invoice.filename, invoice.pair.filename, msg, line_a, line_b, element.bank_detailed_name, element.pair.bank_detailed_name))
+
+                if not element.equal_settlement_amount:
+                    msg = 'Settlement Amount does not match'
+                    errors.append(new_error(
+                        invoice.filename, invoice.pair.filename, msg, line_a, line_b, element.settlement_amount, element.pair.settlement_amount))
+
+                if not element.equal_commission_amount_exc_gst:
+                    msg = 'Commission Amount (Excl GST) does not match'
+                    errors.append(new_error(
+                        invoice.filename, invoice.pair.filename, msg, line_a, line_b, element.commission_amount_exc_gst, element.pair.commission_amount_exc_gst))
+
+                if not element.equal_gst_paid:
+                    msg = 'Amount does not match'
+                    errors.append(new_error(
+                        invoice.filename, invoice.pair.filename, msg, line_a, line_b, element.gst_paid, element.pair.gst_paid))
+
+                if not element.equal_total_amount_paid:
+                    msg = 'Total Amount Paid does not match'
+                    errors.append(new_error(
+                        invoice.filename, invoice.pair.filename, msg, line_a, line_b, element.total_amount_paid, element.pair.total_amount_paid))
+
+                if not element.equal_comments:
+                    msg = 'Total Amount Paid does not match'
+                    errors.append(new_error(
+                        invoice.filename, invoice.pair.filename, msg, line_a, line_b, element.comments, element.pair.comments))
+        else:
+            if write_errors:
+                errors.append(new_error(invoice.filename, invoice.pair.filename, 'No corresponding row in commission file', line_a, '', value_a=description))
+            else:
+                errors.append(new_error(invoice.filename, invoice.pair.filename, 'No corresponding row in commission file', '', line_a, value_b=description))
+
+        return errors
 
 
 def comapre_dicts(worksheet, row, row_a, row_b, margin, filename_a, filename_b, fmt_error, tab):
